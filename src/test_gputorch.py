b1814f04af7e74a64efaffa886f97cbf4f5d328f
lazyoracle <contact@anuragsaharoy.me>
2020-10-25 19:42:58 +0000

    script for pytest

---
+++ src/test_gputorch.py
@@ -0,0 +1,13 @@
+import gputorch
+
+def test_cuda_check():
+    assert gputorch.cuda_check() == True
+
+def test_train():
+    total_epochs = 2
+    PATH = "./model.pth"
+    assert gputorch.train(total_epochs, PATH) < 1.3
+
+def test_test():
+    PATH = "./model.pth"
+    assert gputorch.test(PATH) > 40
