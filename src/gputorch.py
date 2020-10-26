92eac1f2086ec68343dcbca28b444a98650a9785
lazyoracle <contact@anuragsaharoy.me>
2020-10-26 07:56:36 +0530

    reduce net width

--- src/gputorch.py
+++ src/gputorch.py
@@ -10,9 +10,9 @@ import getopt
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.conv1 = nn.Conv2d(3, 2, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 16, 5)
+        self.conv2 = nn.Conv2d(2, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
